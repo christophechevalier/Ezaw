c12a0822102520bbd0ed5e27ecbdf247b50daf02
Christophe Chevalier <cchevalier@linagora.com>
2016-10-27 16:47:49 +0200

    Add test e2e for testing protractor if it works fine

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,14 +1,29 @@
 import { EzawPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('ezaw App', function() {
-  let page: EzawPage;
+  let p: EzawPage;
 
   beforeEach(() => {
-    page = new EzawPage();
+    p = new EzawPage();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('should login if user/pwd match and navigate to menu navigation', () => {
+    p.navigateTo('/auth/login');
+
+    p.click('.page-auth .md-tab-header > div:nth-child(2)');
+
+    p.click('.page-auth .md-tab-header > div:nth-child(1)');
+
+    p.fillInput('md-input input[name="username"]', 'admin');
+    p.fillInput('md-input input[name="password"]', 'admin');
+
+    p.click('.page-login button');
+
+    p.click('.page-nav-module .menu');
+
+    p.click('.menu-open button');
+
+    expect(browser.getCurrentUrl()).toMatch('/nav/navigation');
   });
 });
