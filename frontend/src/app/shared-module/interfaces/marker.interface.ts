2472de0b35f481f67048c84fb7158d250226ddcf
Christophe Chevalier <cchevalier@linagora.com>
2016-12-14 00:06:17 +0100

    Update marker interface tmp

--- frontend/src/app/shared-module/interfaces/marker.interface.ts
+++ frontend/src/app/shared-module/interfaces/marker.interface.ts
@@ -2,11 +2,12 @@
 import { TypedRecord } from 'typed-immutable-record';
 
 export interface IMarker {
-  id: number;
-  name: string;
-  title: string;
+  label?: string;
   lat: number;
   lng: number;
+  // duration: number;
+  // commentary: string;
+  draggable: boolean;
 }
 
 export interface IMarkerRecord extends TypedRecord<IMarkerRecord>, IMarker { };
