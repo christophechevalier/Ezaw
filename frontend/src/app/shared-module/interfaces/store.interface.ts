e7cc4ba73b3b87cd72d8795c645f8c46be0ba58a
Christophe Chevalier <cchevalier@linagora.com>
2016-12-14 00:04:19 +0100

    Delete location interface for the moment

--- frontend/src/app/shared-module/interfaces/store.interface.ts
+++ frontend/src/app/shared-module/interfaces/store.interface.ts
@@ -1,12 +1,10 @@
 // our interfaces
 import { IUserRecord } from './user.interface';
 import { IConfigRecord } from './config.interface';
-import { ILocationRecord } from './location.interface';
 import { IMarkerRecord } from './marker.interface';
 
 export interface IStore {
   config: IConfigRecord;
   user: IUserRecord;
-  location: ILocationRecord;
   marker: IMarkerRecord;
 };
