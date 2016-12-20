f6abe4a69715a7324f24f722e9aa5e72be3066cd
Christophe Chevalier <cchevalier@linagora.com>
2016-12-20 20:54:30 +0100

    Update marker buttons object

--- frontend/src/app/features-module/nav-module/marker-module/marker/marker.ts
+++ frontend/src/app/features-module/nav-module/marker-module/marker/marker.ts
@@ -1,8 +1,12 @@
+// interfaces
+import { ETypeMarkers } from './../../../../shared-module/interfaces/navigation.interface'
+
 export class Marker {
 
   constructor(
     public id: number,
     public title: string,
-    public icon: string
+    public icon: string,
+    public markerType: ETypeMarkers
     ) { }
 }
