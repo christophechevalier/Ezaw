8c241fc76838746358827361264bdca8be13ab4c
Christophe Chevalier <cchevalier@linagora.com>
2016-12-20 18:59:08 +0100

    Create a complex method for add marker

--- frontend/src/app/shared-module/services/marker.service.ts
+++ frontend/src/app/shared-module/services/marker.service.ts
@@ -5,20 +5,144 @@ import { Response } from '@angular/http';
 // rxjs
 import { Observable } from 'rxjs';
 
+// our interfaces
+import { IStore } from './../../shared-module/interfaces/store.interface';
+import {
+  IMarker,
+  IMarkers,
+  INavigationRecord,
+  ETypeMarkers,
+  EControlPolices,
+  EControlAccidents,
+  EControlTrafficJams,
+  EControlWarnings,
+  EControlFavorites,
+  EOnTheRoadCauses,
+  ESideRoadCauses,
+  EWeatherCauses
+} from './../../shared-module/interfaces/navigation.interface';
+
 // http interceptor
 import { InterceptorService } from 'ng2-interceptors';
 
 // our environment
 import { environment } from '../../../environments/environment';
 
+// our helpers
+import { generateUuidV4 } from '../../shared-module/helpers/helper';
+
+// description of markers buttons
 import { Marker } from '../../features-module/nav-module/marker-module/marker/marker';
+
+// generate marker
+import { generateMarker } from './../helpers/helper';
 
 @Injectable()
 export class MarkerService {
+  public marker: IMarker;
+  public currentPosLat: number;
+  public currentPosLng: number;
+  public markers: IMarker[] = [];
 
   constructor(private http: InterceptorService) { }
 
+/*  this.initGeoLocation(currentPosLat, currentPosLng);
+  this.markers.push({
+    ...generateMarker(m, currentPosLat, currentPosLng),
+    title: 'Details marker accident :',
+    label: 'Marker Accident',
+    typeMarker: ETypeMarkers.Accident,
+    control: null,
+    warning: null
+  })
+  break;*/
+
+  // Add new marker with all details
+  addMarker($event: MouseEvent, m: IMarker, currentPosLat: number, currentPosLng: number) {
+    switch (m.typeMarker) {
+      case (ETypeMarkers.Police):
+        this.initGeoLocation(currentPosLat, currentPosLng);
+        this.markers.push(Object.assign(
+          generateMarker(m, currentPosLat, currentPosLng),
+          {
+            title: 'Details marker police :',
+            label: 'Marker Police',
+            typeMarker: ETypeMarkers.Police,
+            control: null,
+            warning: null
+          }
+        ));
+        break;
+      case (ETypeMarkers.Accident):
+        this.initGeoLocation(currentPosLat, currentPosLng);
+        this.markers.push(Object.assign(
+          generateMarker(m, currentPosLat, currentPosLng),
+          {
+            title: 'Details marker accident :',
+            label: 'Marker Accident',
+            typeMarker: ETypeMarkers.Accident,
+            control: null,
+            warning: null
+          }
+        ));
+        break;
+      case (ETypeMarkers.TrafficJam):
+        this.initGeoLocation(currentPosLat, currentPosLng);
+        this.markers.push(Object.assign(
+          generateMarker(m, currentPosLat, currentPosLng),
+          {
+            title: 'Details marker traffic jam :',
+            label: 'Marker Traffic Jam',
+            typeMarker: ETypeMarkers.TrafficJam,
+            control: null,
+            warning: null
+          }
+        ));
+        break;
+      case (ETypeMarkers.Warning):
+        this.initGeoLocation(currentPosLat, currentPosLng);
+        this.markers.push(Object.assign(
+          generateMarker(m, currentPosLat, currentPosLng),
+          {
+            title: 'Details marker warning :',
+            label: 'Marker Warning',
+            typeMarker: ETypeMarkers.Warning,
+            control: null,
+            warning: null
+          }
+        ));
+        break;
+
+      default: this.initGeoLocation(currentPosLat, currentPosLng);
+    }
+  }
+
+  // getCurrentPosition with refresh position in real time
+  initGeoLocation(currentPosLat: number, currentPosLng: number) {
+    console.log('geoooooooo');
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(position => {
+        this.currentPosLat = position.coords.latitude;
+        this.currentPosLng = position.coords.longitude;
+
+        this.markers.push(<IMarker>{
+          id: generateUuidV4(),
+          title: 'Details marker user location :',
+          label: 'Marker User',
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          duration: null,
+          draggable: false,
+          typeMarker: ETypeMarkers.User
+        });
+        console.log('User Current Position: ' + '\n' + '- Lat: ' + this.currentPosLat + '\n' + '- Lng: ' + this.currentPosLng);
+      });
+    }
+  }
+
+  // Get the icons markers of sidenav markers
   getMarkers() {
+    console.log('Getting markers for sidenav right');
     return this.http.get('../../../mocks-json/markers.json')
       .map((response: Response) => <Marker[]>response.json().data);
   }
