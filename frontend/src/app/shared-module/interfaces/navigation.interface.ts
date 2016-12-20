4cc91f129e98eb13ca8b1671a0be817779da3da7
Christophe Chevalier <cchevalier@linagora.com>
2016-12-20 18:49:01 +0100

    Refactor navigation interface

--- frontend/src/app/shared-module/interfaces/navigation.interface.ts
+++ frontend/src/app/shared-module/interfaces/navigation.interface.ts
@@ -1,35 +1,36 @@
 // typed-record
 import { TypedRecord } from 'typed-immutable-record';
 
+// types
 export enum ETypeMarkers { Police, Accident, TrafficJam, Warning, GasStation, User };
+
+// controls
 export enum EControlPolices { Visible, Hide, Opposite };
 export enum EControlAccidents { Light, Serious, Opposite };
 export enum EControlTrafficJams { Light, Medium, Huge };
 export enum EControlWarnings { OnTheRoad, SideRoad, Weather };
 export enum EControlFavorites { Home, Workplace, Others };
+
+export enum EControls { EControlPolices, EControlAccidents, EControlTrafficJams, EControlWarnings, EControlFavorites };
+
+// warnings
 export enum EOnTheRoadCauses { ObjectOnTheRoad, RoadWorks, Pothole, CarStopped, DeadAnimals };
 export enum ESideRoadCauses { CarStopped, Animals, MissingSignaling };
 export enum EWeatherCauses { Fog, Hail, Flood, Ice };
 
+export enum EWarnings { EOnTheRoadCauses, ESideRoadCauses, EWeatherCauses };
+
 export interface IMarker {
   id: string;
+  title?: string;
   label?: string;
-  coordinates: {
-    lat: number;
-    lng: number;
-  };
-  // duration: number;
-  // commentary: string;
+  lat: number;
+  lng: number;
+  duration: number;
   draggable: boolean;
   typeMarker: ETypeMarkers;
-  controlPolice: EControlPolices;
-  controlAccidents: EControlAccidents;
-  controlTrafficJams: EControlTrafficJams;
-  controlWarnings: EControlWarnings;
-  controlFavorites: EControlFavorites;
-  onTheRoadCauses: EOnTheRoadCauses;
-  sideRoadCauses: ESideRoadCauses;
-  weatherCauses: EWeatherCauses;
+  control: EControls;
+  warning: EWarnings;
 }
 
 export interface IMarkers {
