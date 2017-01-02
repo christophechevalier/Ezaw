88e464008468bc517bfb3cf3cca38bf7034bc7cc
Christophe Chevalier <cchevalier@linagora.com>
2017-01-02 19:51:15 +0100

    Update navigation and marker components
    - Add new style of the map
    - Select the navigation store and subscribe to markers
    - Clean the markerService unused now
    - Select the marker and push on the map with content details

--- frontend/src/app/features-module/nav-module/marker-module/marker/marker.component.ts
+++ frontend/src/app/features-module/nav-module/marker-module/marker/marker.component.ts
@@ -1,18 +1,26 @@
 // angular module
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
-// ngrx - store
+// nrgx
 import { Store } from '@ngrx/store';
+
+// immutable
+import { List } from 'immutable';
+
+// rxjs
+import { Subscription } from 'rxjs';
 
 // our actions
 import { ConfigActions } from '../../../../shared-module/reducers/config.actions';
+import { NavigationActions } from '../../../../shared-module/reducers/navigation.actions';
 
 // interfaces
 import { IStore } from '../../../../shared-module/interfaces/store.interface';
 import {
   IMarker,
   IMarkers,
-  INavigationRecord,
+  INavigationList,
   ETypeMarkers,
   EControls,
   EWarnings,
@@ -38,35 +46,57 @@ import { MarkerService } from '../../../../shared-module/services/marker.service
   styleUrls: ['./marker.component.scss']
 })
 
-export class MarkerComponent implements OnInit {
-  public marker: IMarker;
-  public currentPosLat: number;
-  public currentPosLng: number;
+export class MarkerComponent implements OnInit, OnDestroy {
   public listMarkers: Marker[];
+  private markerSub: Subscription;
+  public markers: IMarker[] = [];
 
   constructor(
     private store$: Store<IStore>,
+    private router: Router,
+    private route: ActivatedRoute,
     private markerService: MarkerService
-  ) { }
+  ) {
+    this.markerSub =
+      store$.select('navigation')
+        .map((navigationR: INavigationList) => navigationR.toJS())
+        .subscribe(navigation => {
+          this.markers = navigation;
+        });
+  }
 
   closeSidenavRightIfMobile() {
     this.store$.dispatch({ type: ConfigActions.CLOSE_SIDENAV_RIGHT_IF_MOBILE });
   }
 
   ngOnInit() {
+    this.markerSub =
+      this.route.params.subscribe(params => {
+
+      });
+
     this.markerService.getMarkers()
-      .subscribe(listMarkers => this.listMarkers = listMarkers);
+      .subscribe(listMarkers => {
+        this.listMarkers = listMarkers;
+      });
+  }
+
+  ngOnDestroy() {
+    this.markerSub.unsubscribe();
   }
 
   selectedMarker: Marker; // = this.markers[0];
 
-  isSelected(mk: Marker) {
-    return this.selectedMarker === mk;
+  isSelected(m: Marker) {
+    return this.selectedMarker === m;
   }
 
-  selectIt(mk: Marker) {
-    this.selectedMarker = mk;
-    this.markerService.addMarker(mk.markerType, this.currentPosLat, this.currentPosLng);
-    console.log('Marker : ', mk)
+  selectIt(m: Marker) {
+    this.selectedMarker = m;
+    this.store$.dispatch({ type: NavigationActions.FETCH_MARKER, payload: m.markerType });
+  }
+
+  removeIt(m: Marker) {
+     this.store$.dispatch({ type: NavigationActions.REMOVE_MARKER });
   }
 }
--- frontend/src/app/features-module/nav-module/navigation/navigation.component.ts
+++ frontend/src/app/features-module/nav-module/navigation/navigation.component.ts
@@ -5,6 +5,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 // nrgx
 import { Store } from '@ngrx/store';
 
+// immutable
+import { List } from 'immutable';
+
 // rxjs
 import { Subscription } from 'rxjs';
 
@@ -13,7 +16,7 @@ import { IStore } from './../../../shared-module/interfaces/store.interface';
 import {
   IMarker,
   IMarkers,
-  INavigationRecord,
+  INavigationList,
   ETypeMarkers,
   EControls,
   EWarnings,
@@ -25,7 +28,7 @@ import {
   EOnTheRoadCauses,
   ESideRoadCauses,
   EWeatherCauses
- } from './../../../shared-module/interfaces/navigation.interface';
+} from './../../../shared-module/interfaces/navigation.interface';
 
 // Google Map
 import { MouseEvent } from 'angular2-google-maps/core';
@@ -39,24 +42,182 @@ import { MarkerService } from './../../../shared-module/services/marker.service'
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  styles: [`
+    .sebm-google-map-container {
+      min-height: 100vh;
+      height: 100vh;
+      width: 100%;
+    }
+    .custom-map {
+      overflow: hidden;
+      cursor: wait;
+      height: 100%;
+      width: 100%;
+      position: fixed;
+      min-height: 100%;
+      flex: 1 1 auto;
+      bottom: 41px;
+    }
+    .icon-window-marker {
+      width: 20px;
+      height: 20px;
+    }
+  `]
 })
 
 export class NavigationComponent implements OnInit, OnDestroy {
   public marker: IMarker;
   private markerSub: Subscription;
 
-  public mkType: ETypeMarkers;
+  public lat: number;
+  public lng: number;
   public currentPosLat: number;
   public currentPosLng: number;
   public markers: IMarker[] = [];
 
-  // public markers: IMarkers[];
-
-  public zoom: number = 12;
+  public zoom: number = 8;
   public showMap = true;
-  public centerLat: number = 43.548317;
-  public centerLng: number = 1.502877;
+  public centerLat: number = 43.55;
+  public centerLng: number = 1.50;
+  public lightAndDark = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "hue": "#ff4400"
+        },
+        {
+          "saturation": -68
+        },
+        {
+          "lightness": -4
+        },
+        {
+          "gamma": 0.72
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon"
+    },
+    {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "hue": "#0077ff"
+        },
+        {
+          "gamma": 3.1
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "stylers": [
+        {
+          "hue": "#00ccff"
+        },
+        {
+          "gamma": 0.44
+        },
+        {
+          "saturation": -33
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "stylers": [
+        {
+          "hue": "#44ff00"
+        },
+        {
+          "saturation": -23
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "hue": "#007fff"
+        },
+        {
+          "gamma": 0.77
+        },
+        {
+          "saturation": 65
+        },
+        {
+          "lightness": 99
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "gamma": 0.11
+        },
+        {
+          "weight": 5.6
+        },
+        {
+          "saturation": 99
+        },
+        {
+          "hue": "#0091ff"
+        },
+        {
+          "lightness": -86
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "lightness": -48
+        },
+        {
+          "hue": "#ff5e00"
+        },
+        {
+          "gamma": 1.2
+        },
+        {
+          "saturation": -23
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "saturation": -64
+        },
+        {
+          "hue": "#ff9100"
+        },
+        {
+          "lightness": 16
+        },
+        {
+          "gamma": 0.47
+        },
+        {
+          "weight": 2.7
+        }
+      ]
+    }
+  ];
 
   constructor(
     private store$: Store<IStore>,
@@ -65,9 +226,11 @@ export class NavigationComponent implements OnInit, OnDestroy {
     private markerService: MarkerService
   ) {
     this.markerSub =
-      store$.select('marker')
-        .map((navigationR: INavigationRecord) => navigationR)
-        .subscribe((marker: IMarker) => this.marker = marker);
+      store$.select('navigation')
+        .map((navigationR: INavigationList) => navigationR.toJS())
+        .subscribe(navigation => {
+          this.markers = navigation;
+        });
   }
 
   ngOnInit() {
@@ -75,9 +238,6 @@ export class NavigationComponent implements OnInit, OnDestroy {
       this.route.params.subscribe(params => {
 
       });
-    this.markerService.initGeoLocation(this.currentPosLat, this.currentPosLng);
-    this.markerService.currentUserPosition(this.mkType, this.currentPosLat, this.currentPosLng);
-    // console.log('User Current Position : ' + '\n' + '- Type: ' + this.mkType + '\n' + '- Lat: ' + this.currentPosLat + '\n' + '- Lng: ' + this.currentPosLng);
   }
 
   ngOnDestroy() {
@@ -85,10 +245,10 @@ export class NavigationComponent implements OnInit, OnDestroy {
   }
 
   markerDragEnd($event: MouseEvent) {
-    console.log('Position of the marker updated after drag: ' + '\n', $event);
+
   }
 
   clickedMarker(m: IMarker) {
-    console.log(`Clicked on the marker: ${ m }`);
+
   }
 }
